fc8dc230678ce9b011e2175ec78b97399310c77a
Reaper Gelera <ahoy@barelyhuman.dev>
2024-07-08 01:37:23 +0530

    user domain, migrate func

--- core/src/plugins/domain/domain.ts
+++ core/src/plugins/domain/domain.ts
@@ -1,5 +1,10 @@
 import fp from 'fastify-plugin'
-import { fetchUserById, createProfile } from './knex/user'
+import {
+  fetchUserById,
+  createProfile,
+  searchUsers,
+  updateProfile,
+} from './knex/user.js'
 
 export type Domain = ReturnType<typeof getDomain>
 
@@ -7,7 +12,12 @@ function getDomain(fastify) {
   return {
     user: {
       fetchUserById: (id: number) => fetchUserById(fastify.knex, id),
-      createUser: () => createProfile()
+      createProfile: (email: string, name?: string) =>
+        createProfile(fastify.knex, email, name),
+      updateProfile: (id: number, email: string, name: string) =>
+        updateProfile(fastify.knex, id, email, name),
+      searchUsers: (searchTerm: string) =>
+        searchUsers(fastify.knex, searchTerm),
     },
   }
 }
--- core/src/plugins/domain/knex/user.ts
+++ core/src/plugins/domain/knex/user.ts
@@ -16,6 +16,27 @@ export const createProfile = (db: Knex, email: string, name?: string) => {
   return db('profile').insert({ email, name }).returning(['id'])
 }
 
-export const updateProfile = (db: Knex, email: string, name: string) => {
-  return db('profile').update({ name }).where({ email }).returning(['id'])
+export const updateProfile = (
+  db: Knex,
+  id: number,
+  email: string,
+  name: string
+) => {
+  return db('profile').update({ name, email }).where({ id }).returning(['id'])
+}
+
+export const searchUsers = async (db: Knex, searchTerm: string) => {
+  const normalisedSearchTerm = `%${searchTerm.replace(/\s*/g, '%')}%`
+
+  const users = await db('users')
+    .where('users.email', 'ilike', normalisedSearchTerm)
+    .orWhere('profiles.name', 'ilike', normalisedSearchTerm)
+    .leftJoin('profiles', 'profiles.user_id', 'users.id')
+    .select('users.email', 'users.id', 'profiles.name as profile_name')
+
+  return users
+}
+
+export const deleteUser = (db: Knex, id: string) => {
+  return db('users').where({ id }).del()
 }
